eb777871d030da89980827224e65d815427aacc4
Jorge Bo <jorge.bo@hellofresh.com>
2024-05-10 21:11:03 +0200

    initial commit

---
+++ app/src/main/java/org/example/app/App.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package org.example.app;
+
+import org.example.list.LinkedList;
+
+import static org.example.utilities.StringUtils.join;
+import static org.example.utilities.StringUtils.split;
+import static org.example.app.MessageUtils.getMessage;
+
+import org.apache.commons.text.WordUtils;
+
+public class App {
+    public static void main(String[] args) {
+        LinkedList tokens;
+        tokens = split(getMessage());
+        String result = join(tokens);
+        System.out.println(WordUtils.capitalize(result));
+    }
+}
