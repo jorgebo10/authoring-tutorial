5b3bb53b1013de502797eac6acfbf45271ffad35
Jorge Bo <jorge.bo@hellofresh.com>
2024-05-14 17:21:31 +0200

    add workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.7/userguide/multi_project_builds.html in the Gradle documentation.
  */
-
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
@@ -13,3 +12,5 @@ plugins {
 rootProject.name = "authoring-tutorial"
 include("app", "list", "utilities") //Add modules
 includeBuild("gradle/license-plugin") // Add the new composite build
+
+println("SETTINGS FILE: This is executed during the initialization phase")
